57b798543c2bd5c4c337bf325b7f8c76b0eb6150
Ivan Mashintsev <mashintsev@gmail.com>
2024-04-04 13:24:17 +0300

    hotfix: ErrorCollectionException message

--- src/main/java/ru/mail/jira/plugins/myteam/bot/rulesengine/models/exceptions/ErrorCollectionException.java
+++ src/main/java/ru/mail/jira/plugins/myteam/bot/rulesengine/models/exceptions/ErrorCollectionException.java
@@ -12,9 +12,10 @@ public abstract class ErrorCollectionException extends Exception {
 
     super(
         String.format(
-            "%s%n%s%n%n%s",
+            "%s%n%s%n%s%n%n%s",
             message,
             errors.getReasons().stream().map(Enum::name).collect(Collectors.joining("\n")),
+            String.join("\n", errors.getErrorMessages()),
             errors.getErrors().keySet().stream()
                 .map(e -> String.format("%s: %s", e, errors.getErrors().get(e)))
                 .collect(Collectors.joining("\n"))));
